2d78d0172b007b342735c3e3f0ab7e8c42fedbd3
Lakrandi-Ayeshani <lakrandiayeshani1204@gmail.com>
2024-10-12 12:49:20 +0530

    student controller and service cs added

---
+++ EduConnect/Controllers/StudentsController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using EduConnect.Models;
+using MongoDB.Driver;
+using EduConnect.Services;
+
+namespace EduConnect.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        //private readonly StudentContext _context;
+        private readonly StudentsService _studentsService;
+
+        //public StudentsController(StudentContext context)
+        public StudentsController(StudentsService studentsService)
+        {
+            ///_context = context;
+            _studentsService = studentsService;
+        }
+
+        [HttpGet]
+        public async Task<List<Student>> Get() =>
+           await _studentsService.GetAsync();
+
+
+        [HttpGet("{id:length(24)}")]
+        public async Task<ActionResult<Student>> Get(string id)
+        {
+            var student = await _studentsService.GetAsync(id);
+
+            if (student is null)
+            {
+                return NotFound();
+            }
+
+            return student;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(Student newStudent)
+        {
+            await _studentsService.CreateAsync(newStudent);
+
+            return CreatedAtAction(nameof(Get), new { id = newStudent.Id }, newStudent);
+        }
+
+        // GET: api/Students
+        /* [HttpGet]
+         public async Task<ActionResult<IEnumerable<Student>>> GetStudents()
+         {
+             return await _context.Students.ToListAsync();
+         }
+
+        // GET: api/Students/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Student>> GetStudent(string id)
+        {
+            var student = await _context.Students.FindAsync(id);
+
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return student;
+        }
+
+        // PUT: api/Students/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutStudent(string id, Student student)
+        {
+            if (id != student.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(student).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!StudentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Students
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Student>> PostStudent(Student student)
+        {
+            _context.Students.Add(student);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (StudentExists(student.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetStudent", new { id = student.Id }, student);
+        }
+
+        // DELETE: api/Students/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteStudent(string id)
+        {
+            var student = await _context.Students.FindAsync(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            _context.Students.Remove(student);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool StudentExists(string id)
+        {
+            return _context.Students.Any(e => e.Id == id);
+        }
+
+        */
+    }
+}
---
+++ EduConnect/Services/StudentsService.cs
@@ -0,0 +1,40 @@
+﻿using EduConnect.Models;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace EduConnect.Services
+{
+    public class StudentsService
+    {
+        private readonly IMongoCollection<Student> _studentsCollection;
+
+        public StudentsService(
+            IOptions<StudentDatabaseSettings> studentDatabaseSettings)
+        {
+            var mongoClient = new MongoClient(
+                studentDatabaseSettings.Value.ConnectionString);
+
+            var mongoDatabase = mongoClient.GetDatabase(
+                studentDatabaseSettings.Value.DatabaseName);
+
+            _studentsCollection = mongoDatabase.GetCollection<Student>(
+                studentDatabaseSettings.Value.StudentsCollectionName);
+        }
+
+        public async Task<List<Student>> GetAsync() =>
+            await _studentsCollection.Find(_ => true).ToListAsync();
+
+        public async Task<Student?> GetAsync(string id) =>
+            await _studentsCollection.Find(x => x.Id == id).FirstOrDefaultAsync();
+
+        public async Task CreateAsync(Student newStudent) =>
+            await _studentsCollection.InsertOneAsync(newStudent);
+
+        public async Task UpdateAsync(string id, Student updatedStudent) =>
+            await _studentsCollection.ReplaceOneAsync(x => x.Id == id, updatedStudent);
+
+        public async Task RemoveAsync(string id) =>
+            await _studentsCollection.DeleteOneAsync(x => x.Id == id);
+
+    }
+}
